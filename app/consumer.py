a361935b9ebd620d356b1e24f0fd505608debb10
Aleix Tulko <alexeitulko@gmail.com>
2025-08-11 22:28:38 +0200

    id

--- app/consumer.py
+++ app/consumer.py
@@ -116,13 +116,14 @@ def consume_orders(consumer: Consumer) -> None:
                     for field in value.keys())
             ):
 
+                id = value.get('user_id')
                 name = user_id_map.get(value.get('user_id'))
                 if name is None:
                     name = fetch_user_name(id)
                     if name:
                         user_id_map[id] = name
                     else:
-                        name = f"id={id}"
+                        name = f'id={id}'
                 logger.info(msg=LoggerMsg.ORDER_RECORD.format(
                     client=name,
                     product_name=value.get('product_name'),
